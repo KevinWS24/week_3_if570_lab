745d25ef3e00f5b4611408d34d8a35f787673e3d
KevinWS24 <kevin.sumargo@student.umn.ac.id>
2023-03-03 20:23:27 +0700

    Comparing abstract classes and interface

--- src/main/kotlin/ example/myapp/Main.kt
+++ src/main/kotlin/ example/myapp/Main.kt
@@ -1,9 +1,13 @@
 package example.myapp
+
 fun buildAquarium() {
+//fun buildAquarium() {
 //    val myAquarium = Aquarium()
 //    myAquarium.printSize()
 //    myAquarium.height = 60
 //    myAquarium.printSize()
+
+//
 //    val aquarium1 = Aquarium()
 //    aquarium1.printSize()
 //    // default height and length
@@ -15,10 +19,6 @@ fun buildAquarium() {
 //    // everything custom
 //    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
 //    aquarium4.printSize()
-    val aquarium6 = Aquarium(numberOfFish = 29)
-    aquarium6.printSize()
-    aquarium6.volume = 70
-    aquarium6.printSize()
 //    val aquarium6 = Aquarium(numberOfFish = 29)
 //    aquarium6.printSize()
 //    aquarium6.volume = 70
@@ -29,12 +29,34 @@ fun buildAquarium() {
     myAquarium.printSize()
     val myTower = TowerTank(diameter = 25, height = 40)
     myTower.printSize()
+//    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
+//    myAquarium.printSize()
+//    val myTower = TowerTank(diameter = 25, height = 40)
+//    myTower.printSize()
+//
+//}
+//fun main() {
+//    buildAquarium()
+//}
+//
+//class Main {
+//}
 
 }
+fun makeFish() {
+    val shark = Shark()
+    val pleco = Plecostomus()
+    println("Shark: ${shark.color}")
+    shark.eat()
+    println("Plecostomus: ${pleco.color}")
+    pleco.eat()
 
 
-fun main() {
-    buildAquarium()
-}
-class Main {
-}
+    fun main() {
+        buildAquarium()
+    }
+
+    class Main {
+        fun main () {
+            makeFish()
+        }
